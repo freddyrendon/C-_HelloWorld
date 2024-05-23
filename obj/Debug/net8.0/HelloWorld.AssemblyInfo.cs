a67852ead5163557d1bfc50cabefe3db1b9dfe82
Freddy Rendon <frendonjr03@gmail.com>
2024-05-22 20:15:38 -0400

    add gitignore

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4e48fb6cf42f14d90475653bdfb4eec78c6d92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07bd10602f33e89c47ec840de0869a563998aeac")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
